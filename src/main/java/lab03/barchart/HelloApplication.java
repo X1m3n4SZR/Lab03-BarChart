2e36716630746c0bb8e333959b3eb213413ec231
X1m3n4SZR <sofia@zamoras.me>
2026-09-16 21:49:30 -0700

    got demo code onto project

--- src/main/java/lab03/barchart/HelloApplication.java
+++ src/main/java/lab03/barchart/HelloApplication.java
@@ -3,6 +3,10 @@ package lab03.barchart;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +15,20 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Pane base = new Pane(); //fxmlloader.load();
+        Scene scene = new Scene(base, 320, 240);
+
+        Rectangle bar = new Rectangle(30, 20, Color.AZURE);
+        base.getChildren().add(bar);
+
+        Button redraw = new Button();
+        redraw.setText("Redraw");
+        redraw.setOnAction(actionEvent -> {
+            System.out.println("Redraw Clicked");
+        });
+        base.getChildren().add(redraw);
+
+        stage.setTitle("Lab03: Bar Graph");
         stage.setScene(scene);
         stage.show();
     }
