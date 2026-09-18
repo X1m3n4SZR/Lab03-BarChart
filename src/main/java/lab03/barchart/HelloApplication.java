844ee7b71fac9fc2384770a947562d325afb0d05
X1m3n4SZR <sofia@zamoras.me>
2026-09-17 17:10:02 -0700

    finished demo code and got 3 bars to show up at random positions

--- src/main/java/lab03/barchart/HelloApplication.java
+++ src/main/java/lab03/barchart/HelloApplication.java
@@ -10,27 +10,51 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class HelloApplication extends Application {
+    Rectangle bar1, bar2, bar3;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Pane base = new Pane(); //fxmlloader.load();
         Scene scene = new Scene(base, 420, 440);
 
-        Rectangle bar = new Rectangle(30, 20, Color.AZURE);
-        base.getChildren().add(bar);
+        bar1 = new Rectangle(20, 350, Color.BLUE);
+        base.getChildren().add(bar1);
+
+        bar2 = new Rectangle(20, 350, Color.PURPLE);
+        base.getChildren().add(bar2);
+
+        bar3 = new Rectangle(20, 350, Color.HOTPINK);
+        base.getChildren().add(bar3);
 
         Button redraw = new Button();
         redraw.setText("Redraw");
         redraw.setOnAction(actionEvent -> {
-            System.out.println("Redraw Clicked");
+            onRedraw();
         });
+        redraw.setLayoutX(190);
+        redraw.setLayoutY(390);
         base.getChildren().add(redraw);
 
-        stage.setTitle("Lab03: Bar Graph");
+        onRedraw();
+
+        stage.setTitle("Lab03: Bar Chart");
         stage.setScene(scene);
         stage.show();
+    }
+
+    Random random = new Random();
+    void onRedraw(){
+        bar1.setLayoutX(random.nextDouble()*350);
+        bar1.setLayoutY(20);
+
+        bar2.setLayoutX(random.nextDouble()*350);
+        bar2.setLayoutY(20);
+
+        bar3.setLayoutX(random.nextDouble()*350);
+        bar3.setLayoutY(20);
     }
 
     public static void main(String[] args) {
